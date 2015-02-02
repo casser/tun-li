5fc19cb4666c397b9dffd81b269029a50cde59a2
Sergey Mamyan <sergey.mamyan@gmail.com>
2015-02-02 18:00:02 +0400

    Test

--- Sample Project/MBXMapKit iOS/MBXAppDelegate.m
+++ Sample Project/MBXMapKit iOS/MBXAppDelegate.m
@@ -12,6 +12,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // Test
+    
     return YES;
     
 }
